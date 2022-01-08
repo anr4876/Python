a723f11ec1758f6bf46cba3cb0de3fd1a28bbc4e
anr4876 <dustjdanr@naver.com>
2022-01-08 23:58:13 +0900

    lotto_algorism

---
+++ 2022-01-08/lotto.py
@@ -0,0 +1,32 @@
+def solution(lottos, win_nums ) :
+    answer = []
+    return answer
+
+def randum_lotto() : 
+    for i in range(1, 46) :
+        number_list = [i]
+        
+    randum_lotto_number = random.sample(number_list, 6)
+# ===================================================================   
+# 1~Maximum 까지의 랜덤 번호 num자리       
+# ===================================================================    
+
+import random
+
+def lotto_random_number(maximum, num) :
+    i = 1
+    list1 = []
+    
+    while i <= maximum :
+        list1.append(t1.i)
+        i += 1
+    return random.sample(list1, num)
+
+
+    
+ 
+        
+        
+    
+    
+    
--- test.py
+++ test.py
@@ -1,30 +1,9 @@
-list1 = [2, 5, 9, 11, 23, 51, 26, 38, 41, 93, 101]
-
-
-
-for i in range(10) :
-    print(list1[i], end=" ")
-    
-    
-for i in range(10) :
-    if list1[i] % 2 != 0 :
-        print(list1[i], end=" ")
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+number = 1
+number1 = []
+while number <= 6 :
+    number1.append(number)
+    number += 1
+print(number1)
 
 
 
