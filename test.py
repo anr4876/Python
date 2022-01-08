e3cf3b80ff88d66e0559877306651d8a143a92f9
anr4876 <dustjdanr@naver.com>
2022-01-09 00:12:10 +0900

    lotto_algorism

--- test.py
+++ test.py
@@ -1,14 +1,42 @@
-number = 1
-number1 = []
-while number <= 6 :
-    number1.append(number)
-    number += 1
-print(number1)
 
 
 
 
 
+
+lottos1 = [1,3,4,5,6,7]
+win_nums1 = [1,2,3,4,5,6]
+
+
+
+
+def solution(lottos, win_nums):
+    success_number = 0
+    for num1 in lottos :
+        for num2 in win_nums :
+            if num1 == num2 :
+                success_number += 1 # 민우의 로또 번호와 당첨 번호가 같은 개수
+    number1 = []
+    number = 0
+    while number <= len(win_nums) :
+        number1.append(number) 
+        number += 1
+    
+    number1.reverse()
+    
+    for num in range(0,len(win_nums)+1)  :
+        if success_number == num :
+            win_number = number1[num] # 당첨 등수
+        else :
+            pass
+
+
+    answer = [success_number, win_number]
+    return answer
+
+
+
+print(solution(lottos1, win_nums1))
 
 
 
