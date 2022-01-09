e5f741be9833126cc5c449b74689f9dad0b04ce6
anr4876 <dustjdanr@naver.com>
2022-01-09 23:54:52 +0900

    lotto_algorithm

--- test.py
+++ test.py
@@ -4,42 +4,49 @@
 
 
 
-lottos1 = [1,3,4,5,6,7]
-win_nums1 = [1,2,3,4,5,6]
+lottos1 = [2,4,0,0,7,8]
+win_nums1 = [2,4,5,6,7,8]
 
 
 
 
 def solution(lottos, win_nums):
+    win_number = 0
     success_number = 0
+    secret_number = 0
     for num1 in lottos :
-        for num2 in win_nums :
-            if num1 == num2 :
-                success_number += 1 # 민우의 로또 번호와 당첨 번호가 같은 개수
+        if num1 == 0 :
+            secret_number += 1 # lottos 에 모르는 숫자의 개수를 구하는 함수
+        else :
+            for num2 in win_nums : 
+                if num1 == num2 :  
+                    success_number += 1 # 민우의 로또 번호와 당첨 번호가 같은 개수
+                else :
+                    pass
+    print(secret_number)
+    print(success_number)
     number1 = []
-    number = 0
+    number = 1
     while number <= len(win_nums) :
         number1.append(number) 
         number += 1
     
-    number1.reverse()
+           
     
-    for num in range(0,len(win_nums)+1)  :
-        if success_number == num :
-            win_number = number1[num] # 당첨 등수
-        else :
-            pass
-
-
-    answer = [success_number, win_number]
+    
+    number1.reverse()
+    print(number1)
+    win_number = number1[success_number - 1]
+            
+    top_score = number1[success_number - 1 + secret_number]
+    answer = [win_number, top_score]
     return answer
 
 
 
 print(solution(lottos1, win_nums1))
 
-
-
+# 예시에 [a, b] 안에 들어가는것은 a등에서 b등까지 가능하다뜻 따라서 몇등이 가능한지 예상하는 코드를 작성해야함
 
 
 
